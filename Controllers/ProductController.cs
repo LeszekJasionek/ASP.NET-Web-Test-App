ff3388fa470757c805bcd5388e32c6886b57a18f
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-10 15:59:44 +0100

    Adding ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_test_app.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

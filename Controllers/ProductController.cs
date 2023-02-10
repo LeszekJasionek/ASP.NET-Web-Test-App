fffa88d9ffb99964b4e11963f567cc1f9ad28bc1
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-10 19:06:39 +0100

    Adding Strong View Typing

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using web_test_app.Models;
 
 namespace web_test_app.Controllers
 {
@@ -8,5 +10,52 @@ namespace web_test_app.Controllers
         {
             return View();
         }
+
+        public IActionResult Product()
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Category = "Car",
+                Description = "It's a nice car",
+                Name = "BMW M5"
+            };
+
+            return View(product);
+
+        }
+
+        public IActionResult List()
+        {
+            var productList = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Category = "Car",
+                    Description = "It's a nice car",
+                    Name = "BMW M5"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Category = "Drinks",
+                    Description = "It is very refreshing",
+                    Name = "VW Golf 8"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Category = "Plant",
+                    Description = "It's a prickly plant ",
+                    Name = "The Cactus"
+                },
+
+            };
+
+            return View(productList);
+
+        }
+
     }
 }

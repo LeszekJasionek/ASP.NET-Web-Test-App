78b0f4f49c26878119dc704923c69d1a42312231
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-10 16:31:05 +0100

    Adding models : Product, Category

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace web_test_app.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace web_test_app.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+}

4e62031710467be2eca377f8389a8648377991c5
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-10 15:06:09 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using web_test_app.Models;
+
+namespace web_test_app.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Product(string name)
+        {
+            return View();
+        }
+
+        public IActionResult
+
+    }
+}

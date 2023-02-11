39b93bf7a728a1e67b14ec2d8566b7b1af73793f
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-11 15:49:39 +0100

    Adding partial

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,7 +5,7 @@ namespace web_test_app.Models
     public class Product
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "A Field 'Name' must be filled")]
         public string Name { get; set; }
         [Required]
         public string Description { get; set; }

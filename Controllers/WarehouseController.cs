74e43a8335f125e953e539ac79b2acd54a920b32
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-11 14:59:31 +0100

    Adding more explanation

--- Controllers/WarehouseController.cs
+++ Controllers/WarehouseController.cs
@@ -19,6 +19,16 @@ namespace web_test_app.Controllers
         [HttpPost]
         public IActionResult Add(Product body)
         {
+            //ModelState.IsValid - A statement that determines whether the data that came in agrees with the attributes of a given model
+
+            if(!ModelState.IsValid)
+            {              
+                return View(body);
+
+                //Again, redirection to the view, throw in the values that the user has already entered. And thanks to the fact that we use a model,
+                //the user will keep the data that he has already entered and the data will be entered into the form again.
+            }
+
 
             // logic to write to the product model
 
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,15 @@
-﻿namespace web_test_app.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_test_app.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Category { get; set; }
     }
 }

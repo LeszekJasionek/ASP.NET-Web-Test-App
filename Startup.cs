6c89b1e1d05a840c730023603aa949902aec02e7
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-11 17:51:18 +0100

    Setting DI of EF into Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +31,11 @@ namespace web_test_app
             services.AddControllersWithViews();
 
             services.AddScoped<IWarehouseService, WarehouseService>();
+
+            services.AddDbContext<DbTestContext>(builder =>
+            {
+                builder.UseSqlServer("Server=KOMPUTER02\\SERVERMAIN;Database=WebTestAppDB;Trusted_Connection=True;TrustServerCertificate=true");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

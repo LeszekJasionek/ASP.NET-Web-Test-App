f534907b91e3eb72fc23bf7336010b103b06589a
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-11 14:16:40 +0100

    Making a Form that adds a product to the warehouse

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web_test_app.Models;
+
+namespace web_test_app.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product body)
+        {
+            return View();
+        }
+
+    }
+}
